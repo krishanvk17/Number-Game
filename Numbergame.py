fe200237fd906add613d407c559e20854878f01a
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-19 11:23:15 -0600

    Add files via upload

---
+++ Numbergame.py
@@ -0,0 +1,11 @@
+import random
+import streamlit as st
+st.title("Welcome To The Number Guessing Game!")
+st.info("the computer will pick a number and you have to guess it!")
+userinput=st.text_input("enter a number")
+number=random.randit(1,100)
+if st.button("Guess"):
+	if userinput==random:
+		st.success("You guessed right! refresh to play again")
+	else:
+		st.info("You Guessed wrong!, refresh to get a new number")
