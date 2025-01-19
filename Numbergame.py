a6863acfbd949b71a83aecae631991797a74c3ad
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-19 11:51:38 -0600

    Update Numbergame.py

--- Numbergame.py
+++ Numbergame.py
@@ -5,7 +5,7 @@ st.info("the computer will pick a number and you have to guess it!")
 userinput=st.text_input("enter a number")
 number="17"
 if st.button("Guess"):
-	if userinput==random:
+	if userinput==number:
 		st.success("You Guessed right!")
 	else:
 		st.info("You Guessed wrong! delete number and try again!")
