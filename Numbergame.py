0b38778f0712080ca17bb14be22b2a454277c809
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-19 12:11:22 -0600

    Update Numbergame.py

--- Numbergame.py
+++ Numbergame.py
@@ -10,7 +10,7 @@ if st.button("Guess"):
 		st.success("You Guessed right!")
 	else:
 		st.info("You Guessed wrong! delete number and try again!")
-		if userinput<number1:
+		if userinput<number:
 			st.info("The number is higher than yours")
 		else:
 			st.info("The number is lower than yours")
