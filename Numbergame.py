f0cde35fd58db68b7caaf0bc4bf2fcce8f2ada3d
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-19 11:49:05 -0600

    Update Numbergame.py

--- Numbergame.py
+++ Numbergame.py
@@ -4,10 +4,9 @@ st.title("Welcome To The Number Guessing Game!")
 st.info("the computer will pick a number and you have to guess it!")
 userinput=st.text_input("enter a number")
 number=random.randint(1,100)
-def gameplay(:
-	if st.button("Guess"):
-		if userinput==random:
-			st.success("You guessed right! refresh to play again")
-		else:
-			st.info("You Guessed wrong!, refresh to get a new number")
-gameplay()
+if st.button("Guess"):
+	if userinput==random:
+		st.success("You Guessed right!")
+	else:
+		st.info("You Guessed wrong! delete number and try again!")
+
