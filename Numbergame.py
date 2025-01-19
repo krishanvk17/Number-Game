8b659de6284d5f8949f7b66372521e82e82f2adc
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-19 11:54:08 -0600

    Update Numbergame.py

--- Numbergame.py
+++ Numbergame.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.title("Welcome To The Number Guessing Game!")
 st.info("the computer will pick a number and you have to guess it!")
 userinput=st.text_input("enter a number")
-number="17"
+number1=random.randint(1,100)
+number=str(number1)
 if st.button("Guess"):
 	if userinput==number:
 		st.success("You Guessed right!")
